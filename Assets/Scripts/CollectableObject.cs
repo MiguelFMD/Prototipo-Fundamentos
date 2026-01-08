21095d897847a1b4f12533e1520a43cf19e8e423
Miguel <44586245+MiguelFMD@users.noreply.github.com>
2026-01-08 11:44:37 +0100

    SFX and music

--- Assets/Scripts/CollectableObject.cs
+++ Assets/Scripts/CollectableObject.cs
@@ -3,6 +3,8 @@ using System;
 
 public class CollectableObject : MonoBehaviour
 {
+    [SerializeField] private AudioClip collectSound;
+    [SerializeField, Range(0f, 1f)] private float volume = 0.5f;
     public static event Action OnObjectCollected;
     [SerializeField] private string playerTag = "Player";
 
@@ -10,6 +12,10 @@ public class CollectableObject : MonoBehaviour
     {
         if (collision.CompareTag(playerTag))
         {
+            if (collectSound != null)
+            {
+                AudioSource.PlayClipAtPoint(collectSound, transform.position, volume);
+            }
             OnObjectCollected?.Invoke();
             Destroy(gameObject);
         }
